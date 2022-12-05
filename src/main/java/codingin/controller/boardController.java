8a2a53cf880162a1665b21ab11e5a9e1e5d2dc93
예니 <choeyeeun@naver.com>
2022-12-05 20:23:09 +0900

    12월 5일 PM 20:23 글쓰기,글출력하기 controller , service 작성

--- src/main/java/codingin/controller/boardController.java
+++ src/main/java/codingin/controller/boardController.java
@@ -1,9 +1,17 @@
 package codingin.controller;
 
+import codingin.domain.dto.BoardDto;
+import codingin.service.BoardService;
 import codingin.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/board")
@@ -11,11 +19,49 @@ public class boardController {
     //=============================================//
     @Autowired
     private MemberService memberService;
-//    //=============================================//
-//    @GetMapping("/")    //인덱스 html 불러오기
-//    public Resource index(){
-//        return new ClassPathResource("templates/index.html");
-//    }
-}
+    @Autowired
+    private BoardService boardService;
+//    //====================페이지 [html]요청 로드=========================//
+   @GetMapping("/")    //인덱스 html 불러오기
+   public Resource index(){
+       return new ClassPathResource("templates/index.html");
+    }
 
-//최예은테스트
+    //====================서비스=========================//
+
+    //controller
+    //1. 개별 글쓰기  12.5 최예은
+    // 썸머노트를 사용 할 예정인데 혹시 지장이 있을까요?
+    @PostMapping("/bwrite")
+    public int bwrite(BoardDto boardDto){
+            System.out.println("boardController 1.개별 글쓰기 boardDto 확인 : "+boardDto);
+        int result = boardService.bwrite(boardDto);
+        return result;
+    }
+
+    //2. 글 출력하기 12.5 최예은
+    @GetMapping("/blist")
+    public List<BoardDto> blist(){
+       return boardService.blist();
+    }
+
+
+    //3. 개별 글 보기
+
+
+
+    //4. 모든 글 출력하기
+
+    //5. 글 수정하기
+
+    //6. 글 삭제하기
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/codingin/service/BoardService.java
+++ src/main/java/codingin/service/BoardService.java
@@ -1,8 +1,13 @@
 package codingin.service;
 
+import codingin.domain.dto.BoardDto;
 import codingin.domain.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -20,5 +25,34 @@ public class BoardService {
     @Autowired
     private RereplyRepository rereplyRepository;
     //====================================================//
+
+
+
+    //1. 개별글쓰기 12.5 최예은
+    @Transactional
+    public int bwrite(BoardDto boardDto){
+        BoardEntity boardEntity = boardRepository.save(boardDto.toEntity());
+        return boardEntity.getBno();
+    }
+
+    // 2. 글 출력하기 12.5 최예은
+    public List<BoardDto> blist(){
+        List<BoardEntity> elist = boardRepository.findAll(); //모든엔티티를 꺼내온다
+        List<BoardDto> dlist = new ArrayList<>(); //js는 엔티티를 모르니 dto로 변환 그릇을 미리 만들어 둔다 아직은 깡통이다
+        for(BoardEntity entity : elist){ // 원본에 있는 그릇을 하나씩 dto   그릇에 담아준다.
+            dlist.add(entity.toDto()); //아까 만든 깡통에 하나씩 담아준다.
+        }
+        return  dlist;
+    }
+
+    // 3. 개별  글 보기
+
+    //4. 모든 글 출력하기
+
+    //5. 글 수정하기
+
+    //6. 글 삭제하기
+
+
 
 }
