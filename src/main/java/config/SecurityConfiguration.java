ca32af9c505c7af844df9d5aa2124a1c14dc8c15
rhdmstl <pigji@naver.com>
2022-12-07 10:47:39 +0900

    http보안 설정 확인 후 진행하기

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -1,12 +1,9 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration {
-  /*  @Override
-    protected void configure( httpSecurity http ){
-        http
-
-    }*/
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//http를 통한 보안은 html완성 후 진행
 }
