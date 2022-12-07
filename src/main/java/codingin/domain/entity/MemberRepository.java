46de5eb82e4cc3ede50cc9cde0915a610c74af9a
rhdmstl <pigji@naver.com>
2022-12-07 10:05:05 +0900

    12.07 고은시 이메일 이용한 엔티티 검색 메소드

--- src/main/java/codingin/domain/entity/MemberRepository.java
+++ src/main/java/codingin/domain/entity/MemberRepository.java
@@ -3,6 +3,10 @@ package codingin.domain.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity , Integer> {
+    // 12.07 고은시 1. 이메일 이용한 엔티티 검색 메소드
+    Optional<MemberEntity> findByMemail(String memail );
 }
