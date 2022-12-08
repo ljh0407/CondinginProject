022ce6bf1647a6a905b3fea8d97e7bc56075ae60
예니 <choeyeeun@naver.com>
2022-12-08 16:44:51 +0900

    12월 7일 PM 16:44 글작성 및 썸머노트

--- src/main/java/codingin/controller/boardController.java
+++ src/main/java/codingin/controller/boardController.java
@@ -18,11 +18,20 @@ public class boardController {
     private MemberService memberService;
     @Autowired
     private BoardService boardService;
-//    //====================페이지 [html]요청 로드=========================//
+   //====================페이지 [html]요청 로드=========================//
    @GetMapping("/")    //인덱스 html 불러오기
    public Resource index(){
        return new ClassPathResource("templates/index.html");
     }
+
+    //주혁 test
+    @GetMapping("/updown")
+    public  Resource updown() {return  new ClassPathResource("templates/updown.html");}
+
+    @GetMapping("/bwrite")
+    public  Resource bwrite() {return  new ClassPathResource("templates/board/bwrite.html");}
+
+
 
     //====================서비스=========================//
 
@@ -30,8 +39,8 @@ public class boardController {
     //1. 개별 글쓰기  12.5 최예은
     // 썸머노트를 사용 할 예정인데 혹시 지장이 있을까요?
     @PostMapping("/bwrite")
-    public int bwrite(BoardDto boardDto){
-            System.out.println("boardController 1.개별 글쓰기 boardDto 확인 : "+boardDto);
+    public int bwrite(@RequestBody BoardDto boardDto){ //js에서 json으로 받을 예정이라 @RequestBody로 받아야 함.
+            System.out.println("boardController 1.개별 글쓰기 boardDto 확인 : "+ boardDto);
         int result = boardService.bwrite(boardDto);
             System.out.println("boardController 1. 개별 글 쓰기 result 확인 : " + result);
         return result;
@@ -64,8 +73,7 @@ public class boardController {
        return boardService.deleteboard(bno);
     }
 
-    @GetMapping("/updown")
-    public  Resource updown() {return  new ClassPathResource("templates/updown.html");}
+
 
 
 }
