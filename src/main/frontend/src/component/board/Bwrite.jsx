9d752c560a164599fbc1a8a2693b5a5cfe6ca24b
예니 <choeyeeun@naver.com>
2022-12-13 19:13:52 +0900

    12월 13일 최예은 글쓰기 html-> react로 이동 중

--- src/main/frontend/src/component/board/Bwrite.jsx
+++ src/main/frontend/src/component/board/Bwrite.jsx
@@ -1,29 +1,30 @@
-import React from "react";
+import React,{useState ,useEffect}  from "react";
 import axios from "axios";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';//12.13 최예은 추가
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';//12.13 최예은 추가
 
-
-export default function Bwrite(){
+export default function Bwrite( props ){
     //12.7 최예은 글쓰기 테스트
     alert("bwrite.js 확인")
 
     /*/////////////////////////////글쓰기/////////////////////////////*/
     function bwrite(){
+
         // 한꺼번에 전송하기 위해 객체로 만들어서 가져온다.
-        let data = {
+/*        let data = {
             btitle : document.querySelector(".btitle").value,
             bcontent: document.querySelector(".bcontent").value,
-        }
+        }*/
 
-        console.log("bwrite.js 확인" + data)
-        console.log(data)
+        let boardform = document.querySelector(".boardform");
+        let formdata = new FormData(boardform)
+        formdata.set( "bcontent" ,bcontent ); // 폼데이터의 내용추가ckeditor로 인해 추가 12.13
 
         axios
-            .post("/board/bwrite")
+            .post("/board/bwrite",formdata)
             .then( res => {
                 console.log( res.data )
-                if( res.data == true ){ alert('게시물 작성 성공'); }
+                if( res.data == true ){ alert('게시물 작성 성공'); } // 하고 글 목록 페이지로 넘어가야함
                 else{ alert('게시물 작성 실패'); }
             })
             .catch( err => { console.log( err ); } )
