daeb0c087922fd978205e71506d0550cd07d6a19
이종훈 <dlwhdgns47@naver.com>
2022-12-14 16:54:07 +0900

    이종훈
    쪽지 쪽만 머지 하고 보드는 은시껄로 해주세요!!

--- src/main/frontend/src/component/board/Bwrite.jsx
+++ src/main/frontend/src/component/board/Bwrite.jsx
@@ -3,13 +3,13 @@ import axios from "axios";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-
+const bcontent = '';
 export default function Bwrite(){
     //12.7 최예은 글쓰기 테스트
     alert("bwrite.js 확인")
 
     /*/////////////////////////////글쓰기/////////////////////////////*/
-    function bwrite(){
+    const bwrite = () => {
         // 한꺼번에 전송하기 위해 객체로 만들어서 가져온다.
         let data = {
             btitle : document.querySelector(".btitle").value,
@@ -32,8 +32,12 @@ export default function Bwrite(){
     return(
         <form className="boardform">
             <span>제목</span> <input type="text" name="btitle" className="btitle" id="btitle" /><br/>
-            <span>내용</span> <textarea id="summernote" name="bcontent" className="bcontent" style="resize: none;" /><br/>
-            <button type="button" onClick="bwrite()">작성하기</button>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data=""
+                    onChange={ ( event, editor ) => { const data = editor.getData(); bcontent = data  } }
+                />
+            <button type="button" onClick={bwrite}>작성하기</button>
         </form>
     )
 }
