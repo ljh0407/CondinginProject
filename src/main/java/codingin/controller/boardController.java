8de8bf395129f7dbbf2539bff9521214971ca4d7
rhdmstl <pigji@naver.com>
2022-12-14 17:19:47 +0900

    기능호출네임 통일

--- src/main/java/codingin/controller/boardController.java
+++ src/main/java/codingin/controller/boardController.java
@@ -24,7 +24,7 @@ public class boardController {
     //1. 개별 글쓰기  12.5 최예은
     // 썸머노트를 사용 할 예정인데 혹시 지장이 있을까요?
     @PostMapping("/setboard")
-    public int bwrite(@RequestBody BoardDto boardDto) { //js에서 json으로 받을 예정이라 @RequestBody로 받아야 함.
+    public int bwrite( BoardDto boardDto) { //js에서 json으로 받을 예정이라 @RequestBody로 받아야 함.
         System.out.println("boardController 1.개별 글쓰기 boardDto 확인 : " + boardDto);
         int result = boardService.bwrite(boardDto);
         System.out.println("boardController 1. 개별 글 쓰기 result 확인 : " + result);
