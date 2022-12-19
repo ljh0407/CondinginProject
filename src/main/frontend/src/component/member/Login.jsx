0781f3dc6b0bb52347067d29ca5e07c860748dcb
이태준 <dlxownswnsdn@naver.com>
2022-12-19 17:11:01 +0900

    12/19 헤더 수정함 - 로그인 수정중

--- src/main/frontend/src/component/member/Login.jsx
+++ src/main/frontend/src/component/member/Login.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import StyleSheet from '../../css/login.css'; // css 불러오기
 
 export default function Login (props){
     return(
-        <div>
-            <h3>로그인</h3>
+        <div className="login">
+            <h3 className="title">로그인</h3>
               <form method="post" action="/member/getmember">
-                  <a href="/oauth2/authorization/kakao"> 카카오 로그인 </a>
+                <div id="Log">
+                  <a href="/oauth2/authorization/kakao">  카카오 로그인 </a>
                   <a href="/oauth2/authorization/naver"> 네이버 로그인 </a>
                   <a href="/oauth2/authorization/google"> 구글 로그인 </a>
+                </div>
               </form>
         </div>
     )
