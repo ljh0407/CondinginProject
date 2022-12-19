3e355668cc609a06157d51808597a894b6b70d01
이태준 <dlxownswnsdn@naver.com>
2022-12-19 18:04:20 +0900

    12/19 로그인 수정중

--- src/main/frontend/src/component/member/Login.jsx
+++ src/main/frontend/src/component/member/Login.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import StyleSheet from '../../css/login.css'; // css 불러오기
+import kakao from '../../img/kakao.png';
+import naver from '../../img/naver.png';
+import google from '../../img/google.png';
 
 export default function Login (props){
     return(
         <div className="login">
-            <h3 className="title">로그인</h3>
+            <h3 className="title">Login</h3>
               <form method="post" action="/member/getmember">
                 <div id="Log">
-                  <a href="/oauth2/authorization/kakao">  카카오 로그인 </a>
-                  <a href="/oauth2/authorization/naver"> 네이버 로그인 </a>
-                  <a href="/oauth2/authorization/google"> 구글 로그인 </a>
+                  <a href="/oauth2/authorization/kakao"> <img className="logimg" src={kakao} /> </a>
+                  <a href="/oauth2/authorization/naver"> <img className="logimg" src={naver} /> </a>
+                  <a href="/oauth2/authorization/google"> <img className="logimg" src={google} /> </a>
                 </div>
               </form>
         </div>
