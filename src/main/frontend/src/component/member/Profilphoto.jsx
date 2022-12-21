da0ead88fc4db7240b1d330548507984d913d4fe
이종훈 <dlwhdgns47@naver.com>
2022-12-21 15:53:16 +0900

    12.21 회원정보 출력 url수정 리스트 출력은 다시 확인

--- src/main/frontend/src/component/member/Profilphoto.jsx
+++ src/main/frontend/src/component/member/Profilphoto.jsx
@@ -2,10 +2,10 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function Profilphoto(props){
-    const [member ,setMember] = useState({list:[]});
+    const [member ,setMember] = useState([]);
 
     useEffect(
-        () => axios.get("/member/upprofile").then(res => {setMember(res.data); console.log(res.data)},[]).catch(err => console.log('출력오류 : ' + err))
+        () => axios.get("/member/upprofile").then(res => {setMember(res.data); console.log("프로필"+res.data)},[]).catch(err => console.log('출력오류 : ' + err))
     ,[])
 
     const [memberdto , setMemberDto] = useState({list:[]})
@@ -23,7 +23,7 @@ export default function Profilphoto(props){
                                <td>{m.mprofile}</td>
                                <button type="button" onClick={mupdate}>My Home</button>
                            </tr>
-                   )
+                        )
                    })
                }
            </table>
