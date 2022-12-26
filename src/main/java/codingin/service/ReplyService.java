9862446d39e94e587f7711e57d19f70e2b359265
이종훈 <dlwhdgns47@naver.com>
2022-12-26 15:27:50 +0900

    12-26 고은시 머지완료

--- src/main/java/codingin/service/ReplyService.java
+++ src/main/java/codingin/service/ReplyService.java
@@ -22,7 +22,7 @@ public class ReplyService {
     @Transactional
     public boolean setreply(ReplyDto replyDto , int bno){
         BoardDto boardDto = boardService.getboard(bno); // 상세보기 함수에서 bno 가져오기
-
+        // 이제 시작
         ReplyEntity replyEntity = replyRepository.save(replyDto.toEntity()); // dto entity로 형변환
         return true;
     }
