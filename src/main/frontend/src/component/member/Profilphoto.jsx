7d39f5255d733dffe15ee5758e6d15f4a5a925a4
이종훈 <dlwhdgns47@naver.com>
2022-12-27 15:26:07 +0900

    12.26 로그인 된 회원 정보출력 useState 수정

--- src/main/frontend/src/component/member/Profilphoto.jsx
+++ src/main/frontend/src/component/member/Profilphoto.jsx
@@ -4,16 +4,9 @@ import {useParams} from "react-router-dom";
 
 export default function Profilphoto(props){
 
-    const [member ,setMember] = useState({});
 
-    useEffect(
-        ()=>axios.get("/member/upprofile")
-            .then(res => {setMember(res.data); console.log("1번 : "+res.data)})
-            .catch(err => {console.log('프로필 오류 : '+err)})
-        ,[]
-    )
 
-    const [ login , setLogin ] = useState(null); // 로그인된 회원정보 state 생명주기 // 변경시 재 렌더링
+    const [ login , setLogin ] = useState({}); // 로그인된 회원정보 state 생명주기 // 변경시 재 렌더링
 
     axios
         .get("/member/getloginMno") //url 호출
@@ -23,10 +16,10 @@ export default function Profilphoto(props){
     const mupdate = () => {window.location.href="/member/mupdate"}  //버튼을 눌렀을때 회원수정하기 페이지 이동
 
     return(
-        <div>
-            Email : <div name="memail">{member.memail}</div>
-            닉네임 : <div name="mnick">{member.mnick}</div>
-            프로필사진 : <img className="mprofileImg" src={"/static/media/"+ member.mfilename } />
+        <div>   {/*로그인된 회원 정보 출력*/}
+            Email : <div name="memail">{login.memail}</div>
+            닉네임 : <div name="mnick">{login.mnick}</div>
+            프로필사진 : <img className="mprofileImg" src={"/static/media/"+ login.mfilename } />
             <button type="button" onClick={ mupdate }>수정하기</button>
         </div>
     )
